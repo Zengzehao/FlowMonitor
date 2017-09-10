91d78e8805f7b6ecddac7dfb220e063d98c7dda9
Zengzehao <993528424@qq.com>
2017-09-10 12:53:28 +0800

    修改 远程连接数据库

--- src/utils/JDBCConnection.java
+++ src/utils/JDBCConnection.java
@@ -7,14 +7,14 @@ import java.sql.DriverManager;
 
 public class JDBCConnection {
     static final String driver = "com.mysql.jdbc.Driver";
-    static final String url = "jdbc:mysql://123.207.242.40:3306/flowonitor";
+    static final String url = "jdbc:mysql://123.207.242.40:3306/flowmonitor";
     static final String username = "root";
     static final String password = "root";
     public static Connection getCon(){
         Connection connection = null;
         try {
             Class.forName(driver);
-            connection =  (Connection) DriverManager.getConnection(url,username,password);
+            connection =  DriverManager.getConnection(url,username,password);
         } catch (Exception e) {
             e.printStackTrace();
         }
