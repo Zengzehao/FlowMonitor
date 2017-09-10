386d00e1bc6e2797b24dd3282cbf6b0ca70d79ae
Zengzehao <993528424@qq.com>
2017-09-10 12:55:56 +0800

    实现插入和查询数据库

--- src/serivice/impl/IpAddressServiceImpl.java
+++ src/serivice/impl/IpAddressServiceImpl.java
@@ -1,18 +1,47 @@
 package serivice.impl;
 
 import entity.IpAddress;
+import entity.Protocol;
 import serivice.IpAddressService;
+import utils.JDBCConnection;
 
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.ArrayList;
 import java.util.List;
 
 public class IpAddressServiceImpl implements IpAddressService {
+    static Connection connection = JDBCConnection.getCon();
+    Statement statement = null;
+    ResultSet resultSet = null;
     @Override
-    public void addIpAddress(IpAddress ipAddress) {
-
+    public void addIpAddress(IpAddress ipAddress) throws Exception{
+        statement = connection.createStatement();
+        String sql = "INSERT INTO IpAddress VALUES (null,"+"\""+ipAddress.getIpAddress()+"\""+","+ipAddress.getCount()+")";
+        System.out.println(sql);
+        //String sql = "INSERT INTO Protocol VALUES (null,'TCP',200)";
+        int flag = statement.executeUpdate(sql);
+        if(flag==1){
+            System.out.println("插入数据库成功！");
+        }else {
+            System.out.println("插入数据库失败！");
+        }
     }
 
     @Override
-    public List<IpAddress> queryAllIpAddress() {
-        return null;
+    public List<IpAddress> queryAllIpAddress() throws Exception{
+        statement = connection.createStatement();
+        String sql = "SELECT * FROM IpAddress";
+        List<IpAddress> ipAddresses = new ArrayList<>();
+        ResultSet result = statement.executeQuery(sql);
+        while (result.next()){
+            IpAddress ipAddress = new IpAddress();
+            ipAddress.setCount(result.getLong("id"));
+            ipAddress.setIpAddress(result.getString("ipaddress"));
+            ipAddress.setCount(result.getLong("count"));
+            ipAddresses.add(ipAddress);
+        }
+        return ipAddresses;
     }
 }
--- src/serivice/impl/ProtocalServiceImpl.java
+++ src/serivice/impl/ProtocalServiceImpl.java
@@ -8,6 +8,7 @@ import utils.JDBCConnection;
 import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.Statement;
+import java.util.ArrayList;
 import java.util.List;
 
 public class ProtocalServiceImpl implements ProtocalService {
@@ -18,18 +19,30 @@ public class ProtocalServiceImpl implements ProtocalService {
     @Override
     public void addProtocal(Protocol protocol) throws Exception{
         statement = connection.createStatement();
-        Boolean flag = statement.execute("INSERT INTO IpAddress VALUES ("+protocol.getProtocolType()+","+protocol.getCount()+")");
-        if(flag){
-            System.out.println("插入数据库与成功！");
-        }else{
-            System.out.println("插入数据库与成功！");
+        String sql = "INSERT INTO Protocol VALUES (null,"+"\""+protocol.getProtocolType()+"\""+","+protocol.getCount()+")";
+        System.out.println(sql);
+        //String sql = "INSERT INTO Protocol VALUES (null,'TCP',200)";
+        int flag = statement.executeUpdate(sql);
+        if(flag==1){
+            System.out.println("插入数据库成功！");
+        }else {
+            System.out.println("插入数据库失败！");
         }
     }
 
     @Override
     public List<Protocol> queryAllProtocal() throws Exception{
         statement = connection.createStatement();
-
-        return null;
+        String sql = "SELECT * FROM Protocol";
+        List<Protocol> protocols = new ArrayList<>();
+        ResultSet result = statement.executeQuery(sql);
+        while (result.next()){
+            Protocol protocol = new Protocol();
+            protocol.setId(result.getLong("id"));
+            protocol.setProtocolType(result.getString("protocol"));
+            protocol.setCount(result.getLong("count"));
+            protocols.add(protocol);
+        }
+        return protocols;
     }
 }
